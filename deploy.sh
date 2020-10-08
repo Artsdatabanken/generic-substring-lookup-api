1b23e54ce5b57d0f30a0f434298de61c633dca01
Myrdrahl <even.juberg@artsdatabanken.no>
2020-10-08 10:56:41 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push artsdatabanken/generic-substring-lookup-api
 
 echo "Deploying new docker image to test..."
+echo ${BRANCH}
 if [ "${BRANCH}" == "master" ]
  then
   curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy generic-substring-lookup-api"}' $slackaddy
