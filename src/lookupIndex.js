bd5002b31585abd8e6f12f6fc887d49bac7db028
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-10-08 10:13:03 +0200

    Support JSONlines source files

--- src/lookupIndex.js
+++ src/lookupIndex.js
@@ -2,37 +2,58 @@ const path = require("path");
 const substringIndex = require("indexed-substring-search");
 const fs = require("fs");
 const log = require("log-less-fancy")();
+const readline = require('readline')
 
 function loadJson(filePath) {
   log.info("Reading " + filePath);
   return JSON.parse(fs.readFileSync(filePath)).items;
 }
 
-function loadIndex(filePath, builder) {
+let id = 0;
+
+// Indexes a single lookup entry
+function index(entry, builder) {
+  id++;
+  builder.idToObject[id] = entry.hit;
+  if (!entry.text) return log.warn("Unknown item: " + JSON.stringify(entry));
+  for (const rec of Object.entries(entry.text)) {
+    const [scoreStr, texts] = rec;
+    const score = parseInt(scoreStr) / 100.0;
+    if (!Number.isFinite(score))
+      return log.warn(`Score "${score}" is not a number.`);
+    if (!Array.isArray(texts))
+      return log.warn(`Key "${url}" does not have correct structure`);
+    texts.forEach(text =>
+      builder.addSentence(text, {
+        key: id,
+        score: score
+      })
+    );
+  }
+}
+
+function indexJson(filePath, builder) {
   log.info("Laster " + filePath);
   const input = loadJson(filePath);
-  let id = 0;
   Object.keys(input).forEach(key => {
     const entry = input[key];
-    id++;
-    builder.idToObject[id] = entry.hit;
-    if (!entry.text) return log.warn("Unknown item: " + JSON.stringify(entry));
-    for (const rec of Object.entries(entry.text)) {
-      const [scoreStr, texts] = rec;
-      const score = parseInt(scoreStr) / 100.0;
-      if (!Number.isFinite(score))
-        return log.warn(`Score "${score}" is not a number.`);
-      if (!Array.isArray(texts))
-        return log.warn(`Key "${url}" does not have correct structure`);
-      texts.forEach(text =>
-        builder.addSentence(text, {
-          key: id,
-          score: score
-        })
-      );
-    }
+    index(entry, builder)
   });
-  log.info("Loading index done.");
+}
+
+function indexJsonLines(filePath, builder) {
+  log.info("Laster " + filePath);
+
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(filePath),
+    console: false
+  });
+
+  readInterface.on('line', function (line) {
+    if (line.length <= 0) return
+    const entry = JSON.parse(line)
+    index(entry, builder);
+  });
 }
 
 module.exports = class lookupIndex {
@@ -44,7 +65,12 @@ module.exports = class lookupIndex {
     builder.idToObject = {};
     fs.readdirSync(dataPath).forEach(fn => {
       if (fn.indexOf("full-text-index") < 0) return;
-      loadIndex(path.join(dataPath, fn), builder);
+      const filepath = path.join(dataPath, fn)
+      if (filepath.endsWith('.json'))
+        indexJson(filepath, builder);
+      else
+        indexJsonLines(filepath, builder);
+      log.info("Done loading index.");
     });
     this.index = builder.buildIndex();
     this.idToObject = builder.idToObject;
