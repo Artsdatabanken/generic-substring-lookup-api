c08f4dd4642901c7d5dec46ff980c8c6603856a4
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-10-08 10:49:51 +0200

    remove ssh copy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,5 @@ docker push artsdatabanken/generic-substring-lookup-api
 echo "Deploying new docker image to test..."
 if [ "${BRANCH}" == "master" ]
  then
-  sshpass -p $scp_pass scp -v -o StrictHostKeyChecking=no $FILENAME $scp_user@$scp_dest/forvaltningsportal.tar.gz
   curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy generic-substring-lookup-api"}' $slackaddy
 fi
